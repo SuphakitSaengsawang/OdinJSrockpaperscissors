d8d635249f9573b539b650250edbfac37d87d5ea
Suphakit_Saengsawang <198593035+SuphakitSaengsawang@users.noreply.github.com>
2026-02-12 18:33:40 +0700

    Adding CPUchoices
    // I initially struggled with using Math.random() because it only returns a numeric value.
    // To solve this, I used variables (const and let) together with if / else if / else statements
    // to map each number to its corresponding choice (Rock, Paper, or Scissors).

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log(`Hello World!`);
+function getComputerChoice() {
+    const cpurandomizer = Math.floor(Math.random() * 3);
+    let Botresult = cpurandomizer;
+
+    if (Botresult === 0) {
+        Botresult = `Rock`;
+    } else if (Botresult === 1) {
+        Botresult = `Paper`;
+    } else {
+        Botresult = `Scissors`;
+    }
+
+    return Botresult
+}
+
+console.log(`Bot threw ${getComputerChoice()}`);
