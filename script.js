62a1fff4614cd5263e64468e11c6047fde0b3e57
Suphakit_Saengsawang <198593035+SuphakitSaengsawang@users.noreply.github.com>
2026-02-12 18:44:14 +0700

    Make some edit on how console throwing back result
    Just improve over all readability to know what bot throwing back to  human

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 function getComputerChoice() {
+    //generate random value. 
     const cpurandomizer = Math.floor(Math.random() * 3);
     let Botresult = cpurandomizer;
 
+    //convert number into my desired result of Rock-paper-scissors.
     if (Botresult === 0) {
         Botresult = `Rock`;
     } else if (Botresult === 1) {
         Botresult = `Paper`;
     } else {
-        Botresult = `Scissors`;
+        Botresult = `Scissor`;
     }
 
     return Botresult
 }
 
-console.log(`Bot threw ${getComputerChoice()}`);
+//check result here!
+console.log(`Bot threw: \"  ${getComputerChoice()}  \"`);
